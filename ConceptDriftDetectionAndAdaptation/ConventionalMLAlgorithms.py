cedffb6631f5f591d181c1a378ee40d0c0e50ad4
OguzhanErogluCs <oguzhan.eroglu.cs@gmail.com>
2025-01-01 21:09:27 +0300

    Optimize LightGBM

--- ConceptDriftDetectionAndAdaptation/ConventionalMLAlgorithms.py
+++ ConceptDriftDetectionAndAdaptation/ConventionalMLAlgorithms.py
@@ -13,6 +13,7 @@ from xgboost import XGBClassifier
 import lightgbm as lgb
 import time
 import warnings
+from hyperopt import hp, fmin, tpe, STATUS_OK, Trials
 
 warnings.filterwarnings("ignore")
 matplotlib.use('TkAgg')
@@ -25,6 +26,7 @@ X_train = df1.iloc[:, :-1].values
 y_train = df1.iloc[:, -1].values
 X_test = df2.iloc[:, :-1].values
 y_test = df2.iloc[:, -1].values
+
 
 # print(df2)
 
@@ -117,3 +119,51 @@ y_test = df2.iloc[:, -1].values
 # end_time = time.time()
 # print(f"Execution time: {end_time - start_time} seconds")
 # plt.show()
+
+# # Define the objective function
+# def objective(params):
+#     params = {
+#         'n_estimators': int(params['n_estimators']),
+#         'max_depth': int(params['max_depth']),
+#         'learning_rate': abs(float(params['learning_rate'])),
+#         "num_leaves": int(params['num_leaves']),
+#         "min_child_samples": int(params['min_child_samples']),
+#     }
+#     clf = lgb.LGBMClassifier(**params)
+#     clf.fit(X_train, y_train)
+#     prediction = clf.predict(X_test)
+#     score = accuracy_score(y_test, prediction)
+#     return {'loss': -score, 'status': STATUS_OK}
+#
+#
+# # Define the hyperparameter configuration space
+# space = {
+#     'n_estimators': hp.quniform('n_estimators', 50, 500, 20),
+#     'max_depth': hp.quniform('max_depth', 5, 50, 1),
+#     "learning_rate": hp.uniform('learning_rate', 0, 1),
+#     "num_leaves": hp.quniform('num_leaves', 100, 2000, 100),
+#     "min_child_samples": hp.quniform('min_child_samples', 10, 50, 5),
+# }
+#
+# # Detect the optimal hyperparameter values
+# best = fmin(fn=objective,
+#             space=space,
+#             algo=tpe.suggest,
+#             max_evals=50)
+# print("LightGBM: Hyperopt estimated optimum {}".format(best))
+
+# Use the optimal hyperparameter values to train the optimized LightGBM model
+clf = lgb.LGBMClassifier(max_depth=14, learning_rate=0.8512957337767638, n_estimators=400,
+                         num_leaves=1400, min_child_samples=35)
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+print(classification_report(y_test, y_pred))
+print("Accuracy: " + str(accuracy_score(y_test, y_pred)))
+
+# Plot the confusion matrix
+cm = confusion_matrix(y_test, y_pred)
+f, ax = plt.subplots(figsize=(5, 5))
+sns.heatmap(cm, annot=True, linewidth=0.5, linecolor="red", fmt=".0f", ax=ax)
+plt.xlabel("y_pred")
+plt.ylabel("y_true")
+plt.show()
